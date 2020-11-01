63cfb0f068103b9ea601d87316ad8e77a5d19853
Igor Kichuk <66531978+kichman77@users.noreply.github.com>
2020-11-01 22:44:05 +0200

    minimal-css

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import './Product.css';
 
- function Product({ imgUrl, name, price, quantity }) {
+// const styles = {
+//   container: {
+//     outline: "1px solid #212121",
+// //встраивание инлайн стилей
+//   },
+// };
+
+function Product({ imgUrl, name, price, quantity }) {
   return (
-    <div>
+    <div className="Product">
       <img src={imgUrl} alt={name} width="640" />
-      <h2>{name}</h2>
+      <h2 className='Product-name'>{name}</h2>
       <p>Price: {price}$</p>
-
       <p>Quantity: {quantity < 50 ? 'Few left' : 'In stock'}</p>
       <button type="button">Add to cart</button>
     </div>
@@ -15,14 +22,15 @@ import PropTypes from 'prop-types'
 }
 
 Product.defaultProps = {
-  imgUrl: 'https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder'
-}
+  imgUrl:
+    'https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder',
+};
 
 Product.propTypes = {
   imgUrl: PropTypes.string,
   name: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
-  quantity: PropTypes.number
+  quantity: PropTypes.number,
 };
 
 export default Product;
